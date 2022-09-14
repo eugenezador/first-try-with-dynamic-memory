57d65af3364dd8a89cc5ecd71769e2e49a899e6b
eugene <eugenezador@gmail.com>
2022-09-14 23:43:00 +0300

    add quick sort

--- sort.c
+++ sort.c
@@ -1,76 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int number(int *n);
-int input(int *a, int n);
-void sort(int *a, int n);
-void output(int *a, int n);
+int array_size(size_t *size);
+int* memory_allocation(size_t size);
+int input(int *array, size_t size);
+void output(int *array, size_t size);
+void quick_sort1(int *number, int first, int last);
+void swap(int *a, int *b);
+int partition(int *array, int low, int high);
+void quick_sort2(int *array, int low, int high);
 
 int main() {
-  int *a;
-  int n;
-  int flag = 0;
-  if (number(&n)) {
+  size_t size;
+  if(!array_size(&size)) {
     printf("n/a");
   } else {
-      a = (int *)calloc(n, sizeof(int));
-      if (NULL == a) {
-          printf("n/a");
-      }
-    if (flag != 0)
-      printf("n/a");
-
-    if (input(a, n)) {
+    int *array = memory_allocation(size);
+    if(array == NULL) {
       printf("n/a");
     } else {
-      sort(a, n);
-      output(a, n);
-    }
-  }
-  free(a);
-  return 0;
-}
-
-int number(int *n) {
-  int flag = 0;
-  char c;
-  if (scanf("%d%c", n, &c) == 2 && c == '\n') {
-    if (*n <= 0) {
-      flag++;
-    }
-  }
-  return flag;
-}
-
-int input(int *a, int n) {
-  char c;
-  for (int i = 0; i < n; i++) {
-    if (scanf("%d%c", a + i, &c) != 2)
-      return 1;
-    if (c != ' ' && c != '\n') {
-      return 1;
-    }
-  }
-  return 0;
-}
-
-void sort(int *a, int n) {
-  int temp;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n - i - 1; j++) {
-      if (a[j] > a[j + 1]) {
-        temp = a[j];
-        a[j] = a[j + 1];
-        a[j + 1] = temp;
+      if(input(array, size)) {
+        quick_sort1(array, 0, size - 1);
+        //quick_sort2(array, 0, size - 1);
+        output(array, size);
+      } else {
+        printf("n/a");
       }
+
+      free(array);
     }
+  }
+
+  return 0;
+}
+
+int array_size(size_t *size) {
+  int f = 1;
+  if(!scanf("%ld", size) && *size <= 0) {
+    f = 0;
+  }
+  return f;
+}
+
+int* memory_allocation(size_t size) {
+  return calloc(size, sizeof(int));
+}
+
+int input(int *array, size_t size) {
+  int f = 1;
+  for(size_t i = 0; i < size; i++){
+    if(!scanf("%d", array++)){
+      f = 0;
+    }
+  }
+  return f;
+}
+
+void output(int *array, size_t size) {
+  printf("%d", array[0]);
+  for(size_t i = 1; i < size; i++) {
+    printf(" %d", array[i]);
   }
 }
 
-void output(int *a, int n) {
-  for (int i = 0; i < n; i++) {
-    printf("%d", a[i]);
-    if (i != n - 1)
-      printf("%c", ' ');
+void quick_sort1(int *array, int low, int high) {
+  int i = low, j = high, x = array[(low + high) / 2];
+  
+    do {
+        while (array[i] < x) i++;
+        while (array[j] > x) j--;
+  
+        if(i <= j) {
+            if (array[i] > array[j]) swap(&array[i], &array[j]);
+            i++;
+            j--;
+        }
+    } while (i <= j);
+  
+    if (i < high)
+        quick_sort1(array, i, high);
+    if (low < j)
+        quick_sort1(array, low, j);
+}
+
+void swap(int *a, int *b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+int partition(int *array, int low, int high) {
+  int pivot = array[high];
+  int i = low - 1;
+  for(int j = low; j < high; j++) {
+    if(array[j] < pivot) {
+      i++;
+      swap(&array[i], &array[j]);
+    }
+  }
+  swap(&array[i + 1], &array[high]);
+  return i + 1;
+}
+
+void quick_sort2(int *array, int low, int high) {
+  if(low < high) {
+    int pivot = partition(array, low, high);
+    quick_sort2(array, low, pivot - 1);
+    quick_sort2(array, pivot + 1, high);
   }
 }
