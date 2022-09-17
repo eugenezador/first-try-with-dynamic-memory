bd22178e80b1d9c668448416f03d6cf76b794a03
eugenezador <eugenezafor@gmail.com>
2022-09-17 23:58:20 +0300

    add calculation of each row max and each col min.

--- matrix_extended.c
+++ matrix_extended.c
@@ -1,106 +1,313 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int array_value(int *n, int *m);
+/*
+Calculate each row max and each col min.
+Output calculated values after matrix output.
+*/
+#define ROWS_MAX 100
+#define COLS_MAX 100
 
-int dynamic_input(int **a, int m, int n);
-void dynamic_optput(int **a, int m, int n);
-void max(int **a, int n, int m);
-void min(int **a, int n, int m);
+int allocation_choice(int* allocation_var_number);
+int array_size(int* rows, int* cols);
+
+void static_allocation_case(int static_array[ROWS_MAX][COLS_MAX], const int rows, const int cols, int** dynamic_array);
+int static_array_input(int static_array[ROWS_MAX][COLS_MAX], const int rows, const int cols);
+void static_array_output(int static_array[ROWS_MAX][COLS_MAX], const int rows, const int cols);
+
+void static_array_to_dynamic(int static_array[ROWS_MAX][COLS_MAX], const int rows, const int cols, int** dynamic_array);
+
+void dynamic_allocation_var_1(int** dynamic_array, const int rows, const int cols);
+int** allocation_var_1(int** dynamic_array, const int rows, const int cols);
+void free_var_1_allocation(int** dynamic_array, const int rows);
+
+void dynamic_allocation_var_2(int** dynamic_array, int* array_value, const int rows, const int cols);
+int** allocation_var_2(int** dynamic_array, int* array_value, const int rows, const int cols);
+void free_var_2_allocation(int** dynamic_array, int* array_value);
+
+void dynamic_allocation_var_3(int** dynamic_array, const int rows, const int cols);
+int** allocation_var_3(int** dynamic_array, const int rows, const int cols);
+
+
+int dynamic_array_input(int** dynamic_array, const int rows, const int cols);
+void dynamic_array_output(int** dynamic_array, const int rows, const int cols);
+
+
+void max_of_row(int** dynamic_array, const int rows, const int cols);
+void min_of_col(int** dynamic_array, const int rows, const int cols);
+
 
 int main() {
-  int x;
-  int n, m;
+	int allocation_var_number;
+	if (!allocation_choice(&allocation_var_number)) {
+		printf("Incorrect allocation number");
+	}
+	else {
+		int rows, cols;
+		if (!array_size(&rows, &cols)) {
+			printf("Incorrect array size input");
+		}
+		else {
+			int static_array[ROWS_MAX][COLS_MAX];
+			int** dynamic_array = NULL;
+			int* array_value = NULL;
 
-  if (array_value(&n, &m)) {
-    printf("n/a");
-  } else {
-    int **a1 = (int **)malloc(m * sizeof(int *));
-    for (int i = 0; i < m; i++)
-      a1[i] = (int *)malloc(n * sizeof(int));
+			switch (allocation_var_number) {
+			case 1:
+				static_allocation_case(static_array, rows, cols, dynamic_array);
+				break;
+			case 2:
+				dynamic_allocation_var_1(dynamic_array, rows, cols);
+				break;
+			case 3:
+				dynamic_allocation_var_2(dynamic_array, array_value, rows, cols);
+				break;
+			case 4:
+				dynamic_allocation_var_3(dynamic_array, rows, cols);
+				break;
+			}
+		}
+	}
 
-    if (dynamic_input(a1, n, m)) {
-      printf("n/a");
-    } else {
-      dynamic_optput(a1, m, n);
-      max(a1, n, m);
-      min(a1, n, m);
-    }
-    for (int i = 0; i < n; i++) {
-      free(a1[i]);
-    }
-    free(a1);
-  }
-  return 0;
+	return 0;
 }
 
-int array_value(int *n, int *m) {
-  int flag = 0;
-  if (scanf("%d %d", n, m) == 2 && !(*n <= 0 && *n <= 0)) {
-    flag = 0;
-  } else {
-    flag++;
-  }
-  return flag;
+int allocation_choice(int* allocation_var_number) {
+	int flag = 0;
+	if (scanf("%d", allocation_var_number) == 1
+		&& *allocation_var_number > 0
+		&& *allocation_var_number < 5) {
+		flag = 1;
+	}
+	return flag;
 }
 
-int dynamic_input(int **a, int m, int n) {
-  int flag = 0;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      if (scanf("%d", &a[i][j]) != 1) {
-        flag++;
-      }
-    }
-  }
-  return flag;
+int array_size(int* rows, int* cols) {
+	int flag = 0;
+	if (scanf("%d %d", rows, cols) == 2 && *rows > 0 && *cols > 0) {
+		flag = 1;
+	}
+	return flag;
 }
 
-void dynamic_optput(int **a, int m, int n) {
-  for (int i = 0; i < m; i++) {
-    for (int j = 0; j < n; j++) {
-      printf("%d", a[i][j]);
-      if (j != m - 1)
-        printf("%c", ' ');
-    }
-    printf("\n");
-  }
+
+void static_allocation_case(int static_array[ROWS_MAX][COLS_MAX], const int rows, const int cols,
+	int ** dynamic_array) {
+	if (static_array_input(static_array, rows, cols)) {
+		static_array_output(static_array, rows, cols);
+		
+		dynamic_array = allocation_var_3(dynamic_array, rows, cols);
+		if (dynamic_array == NULL) {
+			printf("Memory allocation error");
+		}
+		else {
+			static_array_to_dynamic(static_array, rows, cols, dynamic_array);
+			printf("\n");
+			max_of_row(dynamic_array, rows, cols);
+			printf("\n");
+			min_of_col(dynamic_array, rows, cols);
+			free(dynamic_array);
+		}
+	}
+	else {
+		printf("Incorrect input of static array");
+	}
+}
+int static_array_input(int static_array[ROWS_MAX][COLS_MAX], const int rows, const int cols) {
+	int flag = 1;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			if (!scanf("%d", &static_array[i][j])) {
+				flag = 0;
+				break;
+			}
+		}
+	}
+	return flag;
+}
+void static_array_output(int static_array[ROWS_MAX][COLS_MAX], const int rows, const int cols) {
+	for (int i = 0; i < rows; i++) {
+		printf("%d", static_array[i][0]);
+		for (int j = 1; j < cols; j++) {
+			printf(" %d", static_array[i][j]);
+		}
+		if (i != rows - 1) printf("\n");
+	}
 }
 
-void max(int **a, int n, int m) {
-  int max = a[0][0];
-  int b[n];
-  for (int i = 0; i < n; i++) {
-    max = 0;
-    for (int j = 0; j < m; j++) {
-      if (a[i][j] > max) {
-        max = a[i][j];
-      }
-      b[i] = max;
-    }
-  }
-  for (int i = 0; i < n; i++) {
-    printf("%d ", b[i]);
-  }
+void static_array_to_dynamic(int static_array[ROWS_MAX][COLS_MAX], const int rows, const int cols,
+	int** dynamic_array) {
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			dynamic_array[i][j] = static_array[i][j];
+		}
+	}
 }
 
-void min(int **a, int n, int m) {
-  int min = a[0][0];
-  int b[m];
-  for (int i = 0; i < m; i++) {
-    min = 9999;
-    for (int j = 0; j < n; j++) {
-      if (a[j][i] < min) {
-        min = a[i][j];
-      }
-      b[i] = min;
-    }
-  }
+void dynamic_allocation_var_1(int** dynamic_array, const int rows, const int cols) {
 
-  printf("\n");
-  for (int i = 0; i < n; i++) {
-    printf("%d", b[i]);
-    if (i < n - 1)
-      printf(" ");
-  }
+	dynamic_array = allocation_var_1(dynamic_array, rows, cols);
+
+	if (dynamic_array == NULL) {
+		printf("Allocation error");
+	}
+	else {
+
+		if (dynamic_array_input(dynamic_array, rows, cols)) {
+			dynamic_array_output(dynamic_array, rows, cols);
+			printf("\n");
+			max_of_row(dynamic_array, rows, cols);
+			printf("\n");
+			min_of_col(dynamic_array, rows, cols);
+		}
+		else {
+			printf("Incorrect input of dynamic array");
+		}
+
+		free_var_1_allocation(dynamic_array, rows);
+	}
+
 }
+
+int** allocation_var_1(int** dynamic_array, const int rows, const int cols) {
+	dynamic_array = (int**)malloc(rows * sizeof(int*));
+	for (int i = 0; i < rows; i++) {
+		dynamic_array[i] = (int*)malloc(cols * sizeof(int));
+	}
+	return dynamic_array;
+}
+
+void free_var_1_allocation(int** dynamic_array, const int rows) {
+	for (int i = 0; i < rows; i++) {
+		free(dynamic_array[i]);
+	}
+	free(dynamic_array);
+}
+
+void dynamic_allocation_var_2(int** dynamic_array, int* array_value, const int rows, const int cols) {
+
+	dynamic_array = allocation_var_2(dynamic_array, array_value, rows, cols);
+
+	if (dynamic_array == NULL) {
+		printf("Allocation error");
+	}
+	else {
+
+		if (dynamic_array_input(dynamic_array, rows, cols)) {
+			dynamic_array_output(dynamic_array, rows, cols);
+			printf("\n");
+			max_of_row(dynamic_array, rows, cols);
+			printf("\n");
+			min_of_col(dynamic_array, rows, cols);
+		}
+		else {
+			printf("Incorrect input of dynamic array");
+		}
+
+		free_var_2_allocation(dynamic_array, array_value);
+	}
+
+}
+
+int** allocation_var_2(int** dynamic_array, int* array_value, const int rows, const int cols) {
+	dynamic_array = (int**)malloc(rows * sizeof(int*));
+	array_value = (int*)malloc(rows * cols * sizeof(int));
+
+	for (int i = 0; i < rows; i++) {
+		dynamic_array[i] = array_value + rows * i;
+	}
+	return dynamic_array;
+}
+
+void free_var_2_allocation(int** dynamic_array, int* array_value) {
+	free(dynamic_array);
+	free(array_value);
+}
+
+
+void dynamic_allocation_var_3(int** dynamic_array, const int rows, const int cols) {
+
+	dynamic_array = allocation_var_1(dynamic_array, rows, cols);
+
+	if (dynamic_array == NULL) {
+		printf("Allocation error");
+	}
+	else {
+
+		if (dynamic_array_input(dynamic_array, rows, cols)) {
+			dynamic_array_output(dynamic_array, rows, cols);
+			printf("\n");
+			max_of_row(dynamic_array, rows, cols);
+			printf("\n");
+			min_of_col(dynamic_array, rows, cols);
+		}
+		else {
+			printf("Incorrect input of dynamic array");
+		}
+
+		free(dynamic_array);
+	}
+}
+int** allocation_var_3(int** dynamic_array, const int rows, const int cols) {
+
+	dynamic_array = (int**)malloc(rows * sizeof(int*) + rows * cols * sizeof(int));
+	int* ptr = (int*)(dynamic_array + rows);
+
+	for (int i = 0; i < rows; i++) {
+		dynamic_array[i] = ptr + cols * i;
+	}
+
+	return dynamic_array;
+}
+
+int dynamic_array_input(int** dynamic_array, const int rows, const int cols) {
+	int flag = 1;
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			if (!scanf("%d", &dynamic_array[i][j])) {
+				flag = 0;
+				break;
+			}
+		}
+	}
+	return flag;
+}
+
+void dynamic_array_output(int** dynamic_array, const int rows, const int cols) {
+	for (int i = 0; i < rows; i++) {
+		printf("%d", dynamic_array[i][0]);
+		for (int j = 1; j < cols; j++) {
+			printf(" %d", dynamic_array[i][j]);
+		}
+		if(i != rows - 1) printf("\n");
+	}
+}
+
+void max_of_row(int** dynamic_array, const int rows, const int cols) {
+	int max = 0;
+	for (int i = 0; i < rows; i++) {
+		max = dynamic_array[i][0];
+		for (int j = 1; j < cols; j++) {
+			if (dynamic_array[i][j] > max) {
+				max = dynamic_array[i][j];
+			}
+		}
+		if (i != rows - 1) printf("%d ", max);
+		else printf("%d", max);
+	}
+}
+
+void min_of_col(int** dynamic_array, const int rows, const int cols) {
+	int min = 0;
+	for (int j = 0; j < cols; j++) {
+		min = dynamic_array[0][j];
+		for (int i = 1; i < rows; i++) {
+			if (dynamic_array[j][i] < min) {
+				min = dynamic_array[i][j];
+			}
+		}
+		if (j != cols - 1) printf("%d ", min);
+		else printf("%d", min);
+	}
+}
+
